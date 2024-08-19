8f887a2fad3c0775eaef5ddb2ad8323131a94294
MATEO ANSELMO <matteo.anselmo96@gmail.com>
2024-08-19 14:38:44 +0200

    fix issue for different dashboard

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -12,13 +12,11 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
 
-class AuthenticatedSessionController extends Controller
-{
+class AuthenticatedSessionController extends Controller {
     /**
      * Display the login view.
      */
-    public function create(): Response
-    {
+    public function create(): Response {
         return Inertia::render('Auth/Login', [
             'canResetPassword' => Route::has('password.request'),
             'status' => session('status'),
@@ -28,20 +26,26 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
-    {
+    public function store(LoginRequest $request): RedirectResponse {
         $request->authenticate();
 
         $request->session()->regenerate();
+        if (Auth::check()) {
+            $user = Auth::user();
 
+            if ($user->role === 'admin') {
+                return redirect()->intended(RouteServiceProvider::ADMIN);
+            } elseif ($user->role === 'user') {
+                return redirect()->intended(RouteServiceProvider::HOME);
+            }
+        }
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): RedirectResponse
-    {
+    public function destroy(Request $request): RedirectResponse {
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -8,24 +8,23 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class RedirectIfAuthenticated
-{
+class RedirectIfAuthenticated {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string ...$guards): Response
-    {
+    public function handle(Request $request, Closure $next, string ...$guards): Response {
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
                 $user = Auth::guard($guard)->user();
-                if($user->role === 'admin'){
-                    return redirect(RouteServiceProvider::ADMIN);
-                }  elseif ($user->role === 'user') {
-                    return redirect(RouteServiceProvider::HOME);
+
+                if ($user->role === 'admin') {
+                    return redirect()->route('admin.dashboard');
+                } elseif ($user->role === 'user') {
+                    return redirect()->route('dashboard');
                 }
             }
         }
